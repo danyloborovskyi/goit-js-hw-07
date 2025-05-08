cfc93a2a1a382de901c70102aee218718ae0ea23
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-05-08 15:33:03 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+const list = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${list.length}`);
+
+list.forEach(category => {
+    const title = category.querySelector("h2").textContent;
+    const item = category.querySelectorAll("ul li");
+
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${item.length}`);
+})
+
+
+
+
