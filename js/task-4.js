6ac409a124c25f120232b388151ccade52132a16
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-05-12 12:43:05 +0300

    task-4 is done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const signUpForm = document.querySelector(".login-form");
 
+signUpForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const form = event.target;
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (login === "" || password === "") {
+        return alert("All form fields must be filled in");
+    };
+
+    const data = {
+        email: login.trim(),
+        password: password.trim()
+    }
+
+    console.log(data);
+  form.reset();
+}
