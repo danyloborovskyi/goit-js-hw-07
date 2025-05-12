6579d058d7537d8ef4f4167994e50b01c8d80515
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-05-12 15:01:41 +0300

    task-5 is done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const currentColor = document.querySelector(".color");
+const wrapper = document.querySelector(".widget");
+
+button.addEventListener("click", () => {
+  currentColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+});
+
+body.style.display = "flex";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.flexDirection = "column";
+body.style.height = "50vw";
+
+wrapper.style.display = "flex";
+// wrapper.style.justifyContent = "center";
+wrapper.style.alignItems = "center";
+wrapper.style.flexDirection = "column";
+
+button.style.backgroundColor = "#4E75FF";
+
+
+
+
+
+
+
